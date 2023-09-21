9b5ad55335279539b133cf1d8935f295ed8d2649
Prince <prinsharma.9@gmail.com>
2023-09-21 17:19:46 +0530

    Handling Css in microfrontend

--- container/src/App.js
+++ container/src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 const App = () => {
   return (
-    <div>
-      <MarketingApp />
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
