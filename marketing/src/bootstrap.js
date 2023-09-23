8a24c4429fbc2b0cd529d4fb858aa66db83385c3
Prince <prinsharma.9@gmail.com>
2023-09-23 11:49:39 +0530

    Multi-Tier Navigation

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,17 +1,33 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+
 import App from "./App";
 
 //Mount function to start up the app
-const mount = (el) => {
-  ReactDom.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  onNavigate && history.listen(onNavigate);
+
+  ReactDom.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      pathname !== nextPathname && history.push(nextPathname);
+    },
+  };
 };
 
 //In development environment and in isolation, call mount immediately
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
-  devRoot && mount(devRoot);
+  devRoot &&
+    mount(devRoot, {
+      defaultHistory: createBrowserHistory(),
+    });
 }
 
 //Export the mount function
