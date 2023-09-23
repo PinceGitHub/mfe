b86991022a973669430f5eb15b79e28d128f4597
Prince <prinsharma.9@gmail.com>
2023-09-23 16:49:55 +0530

    Final Commit

--- auth/src/bootstrap.js
+++ auth/src/bootstrap.js
@@ -5,7 +5,7 @@ import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 //Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
   const history =
     defaultHistory ||
     createMemoryHistory({
@@ -14,7 +14,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 
   onNavigate && history.listen(onNavigate);
 
-  ReactDom.render(<App history={history} />, el);
+  ReactDom.render(<App history={history} onSignIn={onSignIn} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
@@ -26,7 +26,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 
 //In development environment and in isolation, call mount immediately
 if (process.env.NODE_ENV === "development") {
-  const devRoot = document.querySelector("#_marketing-dev-root");
+  const devRoot = document.querySelector("#_auth-dev-root");
 
   devRoot &&
     mount(devRoot, {
--- container/src/App.js
+++ container/src/App.js
@@ -1,22 +1,50 @@
-import React from "react";
-import MarketingApp from "./components/MarketingApp";
+import React, { lazy, Suspense, useState, useEffect } from "react";
 import Header from "./components/Header";
-import { BrowserRouter } from "react-router-dom";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+import { createBrowserHistory } from "history";
+
+const MarketingAppLazy = lazy(() => import("./components/MarketingApp"));
+const AuthAppLazy = lazy(() => import("./components/AuthApp"));
+const DashboardAppLazy = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
+const history = createBrowserHistory();
+
 const App = () => {
+  const [isSignedIn, setSignedIn] = useState(false);
+
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    }
+  }, [isSignedIn]);
+
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
-          <Header />
-          <MarketingApp />
+          <Header
+            isSignedIn={isSignedIn}
+            onSignOut={() => setSignedIn(false)}
+          />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/auth">
+                <AuthAppLazy onSignIn={() => setSignedIn(true)} />
+              </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardAppLazy />
+              </Route>
+              <Route path="/" component={MarketingAppLazy} />
+            </Switch>
+          </Suspense>
         </div>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 };
